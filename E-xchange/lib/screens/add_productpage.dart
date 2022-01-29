c9e7626f1d9404cad5d4ee9384e3092cbf6c096f
chandresh2702 <chandresh2702@gmail.com>
2022-01-29 17:59:59 +0530

    adding price column in adding product

--- E-xchange/lib/screens/add_productpage.dart
+++ E-xchange/lib/screens/add_productpage.dart
@@ -13,7 +13,8 @@ class _AddProductState extends State<AddProduct> {
     return Scaffold(
       backgroundColor: Colors.black,
       resizeToAvoidBottomInset: false,
-      body: Column(
+      body: SingleChildScrollView(
+        child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Container(
@@ -40,6 +41,7 @@ class _AddProductState extends State<AddProduct> {
             padding: EdgeInsets.only(left: 20, top: 80, right: 20),
             child: Column(children: <Widget>[
               TextField(
+                style: TextStyle(color: Colors.white),
                 decoration: InputDecoration(
                     enabledBorder: OutlineInputBorder(
                         borderSide: const BorderSide(color:Colors.amber)
@@ -56,6 +58,7 @@ class _AddProductState extends State<AddProduct> {
               TextFormField(
                 keyboardType: TextInputType.multiline,
                 maxLines: null,
+                style: TextStyle(color: Colors.white),
                 decoration: InputDecoration(
                     enabledBorder: OutlineInputBorder(
                         borderSide: const BorderSide(color:Colors.amber)
@@ -69,11 +72,30 @@ class _AddProductState extends State<AddProduct> {
                         fontWeight: FontWeight.bold,
                         color: Colors.grey)),
               ),
+              SizedBox(height: 14),
+              TextFormField(
+                style: TextStyle(color: Colors.white),
+                 keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color:Colors.amber)
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color:Colors.amber)
+                    ),
+                    labelText: 'Enter Price',
+                    labelStyle: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey)),
+              ),
+
             ]),
           ),
+
           Container(
             alignment: Alignment.bottomCenter,
-            padding: EdgeInsets.only(left: 30, top: 300, right: 20, bottom: 30),
+            padding: EdgeInsets.only(left: 30, top: 100, right: 20, bottom: 30),
             child: Row(
 
               children: <Widget>[
@@ -109,6 +131,7 @@ class _AddProductState extends State<AddProduct> {
             ),
           )
         ],
+      ),
       ),
     );
   }
